f83e336e1258d27e81adf914232520b0c33e9733
vigneshwarmpresidio <vigneshwarm@presidio.com>
2023-12-28 15:26:55 +0530

    qwdf

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,24 @@
-import os
+import json
 
-print('hi')
+traffic_policy_file = "../templates/r53-weighted-policy.json"
+redirect_region = "secondary"
 
+def load_traffic_policy(policy_file, primary_endpoint, secondary_endpoint):
+  #read the json file
+  with open(policy_file, 'r') as file:
+    data = json.load(file)
 
-print(os.environ.get('MY_VAR'))
+  #replace endpoint values
+  data['Endpoints']['primary-endpoint']['Value'] = primary_endpoint
+  data['Endpoints']['secondary-endpoint']['Value'] = secondary_endpoint
+
+  #assign weights
+  primary_endpoint_weight = 100 if redirect_region == "primary" else 0
+  secondary_endpoint_weight = 100 if redirect_region == "secondary" else 0
+
+  data["Rules"]["weighted-rule"]["Items"][0]["Weight"] = primary_endpoint_weight
+  data["Rules"]["weighted-rule"]["Items"][1]["Weight"] = secondary_endpoint_weight
+
+  return json.dumps(data)
+
+load_traffic_policy(traffic_policy_file, "a", "b")
